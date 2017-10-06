f3579ce5a7304db515c5c049f60f7368684a822a
Brandon <bshoc@bellsouth.net>
2017-10-06 18:37:44 -0400

    Reverted Enemy.cs back to what it was

--- Assets/Scripts/Character/Enemies/Enemy.cs
+++ Assets/Scripts/Character/Enemies/Enemy.cs
@@ -14,7 +14,7 @@
         [SerializeField]
         private int maxHealth = 3;
         [SerializeField]
-        private Enums.EnemyTypes type;
+        private Enums.EnemyTypes type = Enums.EnemyTypes.Basic;
 
         /// <summary> The type of this enemy. </summary>
         public Enums.EnemyTypes Type { get { return this.type; } }
