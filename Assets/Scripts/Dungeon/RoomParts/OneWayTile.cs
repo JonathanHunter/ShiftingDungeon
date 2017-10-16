18c853f7d8548b0da1f9fec8ff8daad16fa02622
Jonathan Hunter <hunterjonathant@gmail.com>
2017-10-16 12:08:01 -0400

    Dungeon obstacles

---
+++ Assets/Scripts/Dungeon/RoomParts/OneWayTile.cs
@@ -0,0 +1,20 @@
+﻿namespace ShiftingDungeon.Dungeon.RoomParts
+{
+    using UnityEngine;
+    using Util;
+
+    public class OneWayTile : MonoBehaviour 
+    {
+        public float pushForce = 2f;
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.gameObject.tag == Enums.Tags.Hero.ToString())
+            {
+                other.gameObject.GetComponent<Rigidbody2D>().AddForce(this.transform.right * this.pushForce, ForceMode2D.Force);
+            }
+            else
+                Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), other);
+        }
+    }
+}
